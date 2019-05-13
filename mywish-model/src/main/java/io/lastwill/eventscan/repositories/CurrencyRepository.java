fa3e5df5195c8d6064cb0229d3fb276ace17cf0a
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-05-13 14:48:59 +0300

    add currency repository

---
+++ mywish-model/src/main/java/io/lastwill/eventscan/repositories/CurrencyRepository.java
@@ -0,0 +1,7 @@
+package io.lastwill.eventscan.repositories;
+
+import io.lastwill.eventscan.model.Currency;
+import org.springframework.data.repository.Repository;
+
+public interface CurrencyRepository extends Repository<Currency, Long> {
+}
